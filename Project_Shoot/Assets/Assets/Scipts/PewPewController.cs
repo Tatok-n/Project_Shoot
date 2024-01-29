31d33610aa974343d644f165db9db62764cfeed3
tnhunter <tnhunter2004@gmail.com>
2024-01-28 21:58:54 -0500

    Improved Shot prediction
    Now the helper overlay will only trigger if the bullet is coming from the appropriate direction

--- Project_Shoot/Assets/Assets/Scipts/PewPewController.cs
+++ Project_Shoot/Assets/Assets/Scipts/PewPewController.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class PewPewController : MonoBehaviour
 {
     public Transform thisboi;
-    public Vector3 thisboipos, newpos;
+    public Vector3 thisboipos, newpos, delta;
 
-    public float front,right,speed;
+    public float front,right,speed,triggerRotation;
 
     public Scoring score;
 
@@ -22,9 +22,28 @@ public class PewPewController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        thisboipos = thisboi.position;
+        thisboipos = thisboi.position;                        // base projectile movement 
         newpos = new Vector3 (thisboipos.x + right*speed, thisboipos.y, thisboipos.z + front*speed);
         thisboi.position = newpos;
+
+        delta = new Vector3((Mathf.Round((thisboipos.x - newpos.x) / speed)), 0f, Mathf.Round((thisboipos.z - newpos.z) / speed)); 
+        
+        
+        if (delta ==new Vector3 (1f,0f, 0f))  //increasing X 
+        {
+            triggerRotation = 90;
+        } else if (delta == new Vector3(-1f, 0f, 0f))
+        {
+            triggerRotation = 270; //decreasing X 
+        } else if (delta == new Vector3(-1f, 0f, 0f)) { //increasing Z 
+            triggerRotation = 0;
+        }
+        else //decreasing Z
+        {
+            triggerRotation = 180;
+        }
+           
+
     }
 
     void OnCollisionEnter(Collision collision) {
@@ -35,7 +54,12 @@ public class PewPewController : MonoBehaviour
         if (other.tag == "Player") {
             score.GameOver();
         } else if (other.tag == "Assist") {
+            if (other.GetComponent<AssistController>().isLeft && triggerRotation==270)
             other.GetComponent<AssistController>().triggered = true;
+            else if (other.GetComponent<AssistController>().isRight && triggerRotation == 90)
+                other.GetComponent<AssistController>().triggered = true;
+            else if (other.GetComponent<AssistController>().isBack && triggerRotation == 180)
+                other.GetComponent<AssistController>().triggered = true;
         } else if (!(other.tag == "Pads" || other.tag == "Targets" || other.tag == "PewPew")) {
             Destroy(this.gameObject);
         } 
